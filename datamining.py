d60bf7b82427755e2408810348be94739df6e6c2
ashleychen628 <ashley.cdy628@gmail.com>
2025-04-22 19:43:22 +0000

    completed rule selection

--- datamining.py
+++ datamining.py
@@ -10,8 +10,8 @@ class DataMining:
 
         # get the transactions in form of list of transactions
         self.transactions = self.read_transactions_from_csv()
-        total_transactions = len(self.transactions)
-        self.support_threshold = float(self.min_sup * total_transactions)
+        self.total_transactions = len(self.transactions)
+        self.support_threshold = float(self.min_sup * self.total_transactions)
 
     
     def start(self):
@@ -26,18 +26,14 @@ class DataMining:
         L1 = []
         for item, count in item_counts.items():
             if count >= self.support_threshold:
-                # L1.append((frozenset([item]), count))
                 L1.append(item)
 
-        # print(item_counts)
-        self.apriori(L1, item_counts)
-        # DEBUG PRINTS
-        # print("L1 frequent 1-itemsets:")
-        # for itemset, count in L1:
-        #     support_percent = 100 * count / total_transactions
-        #     print(f"{list(itemset)}: {support_percent:.2f}%")
+        # Apriori algorithm by min_sup
+        all_frequent_itemsets = self.apriori(L1, item_counts)
+        # select the rules by min_conf
+        rules_selected = self.generate_rules(all_frequent_itemsets)
 
-        # 
+        self.print_output(all_frequent_itemsets, rules_selected)
 
     def read_transactions_from_csv(self):
         """ Read dataset in filename into a list of sets. """
@@ -104,3 +100,53 @@ class DataMining:
         return all_frequent_itemsets
         # for item, count in all_frequent_itemsets.items():
         #     print(f'{item}: {count}')
+    
+    def generate_rules(self, frequent_itemsets):
+        """ Generate the rules using the filtered itemsets. """
+        rules = []
+        for itemset in frequent_itemsets:
+            if len(itemset) < 2:
+                continue  
+
+            # Generate all non-empty proper subsets of the itemset
+            for i in range(1, len(itemset)):
+                for LHS in combinations(itemset, i):
+                    LHS = frozenset(LHS)
+                    RHS = itemset - LHS
+                    if len(RHS) == 1: # exactly one item on the right side
+                        support_LHS_RHS_count = frequent_itemsets[itemset]  # already normalized
+                        support_LHS_count = frequent_itemsets.get(LHS)
+
+                        if support_LHS_count: # at least one item on the left side
+                            confidence = support_LHS_RHS_count / support_LHS_count
+                            support_LHS_RHS = support_LHS_RHS_count / self.total_transactions
+                            if confidence >= self.min_conf:
+                                rules.append((LHS, RHS, support_LHS_RHS, confidence))
+                                # print(f'for A:{LHS}, and B:{RHS}, support_count={support_LHS_RHS_count}, support_AB={support_LHS_RHS}, support_A_count={support_LHS_count}, confidence={confidence}')
+
+        return rules
+
+    def print_output(self, frequent_itemsets, rules):
+        """ Print the output to txt file. """
+        # Sort rules by confidence (descending)
+        sorted_rules = sorted(rules, key=lambda x: x[3], reverse=True)
+
+        # Sort frequent list by support (descending)
+        sorted_frequent = sorted(
+            [(itemset, count) for itemset, count in frequent_itemsets.items()],
+            key=lambda x: x[1], reverse=True
+        )
+
+        # Write to output.txt
+        with open("output.txt", "w", encoding="utf-8") as f:
+            f.write(f"==Frequent itemsets (min_sup={int(self.min_sup * 100)}%)\n")
+            for itemset, count in sorted_frequent:
+                support_percent = 100 * count / self.total_transactions
+                items = ",".join(sorted(itemset))
+                f.write(f"[{items}], {support_percent:.4f}%\n")
+
+            f.write(f"==High-confidence association rules (min_conf={int(self.min_conf * 100)}%)\n")
+            for A, B, support, confidence in sorted_rules:
+                left = ",".join(sorted(A))
+                right = ",".join(sorted(B))
+                f.write(f"[{left}] => [{right}] (Conf: {confidence*100:.1f}%, Supp: {support*100:.4f}%)\n")
